fb16b82f0b8463abc6a424f038e3485c95b4616a
Deniss Akisins <Denjaa3@icloud.com>
2023-05-21 16:28:30 +0300

    Setup labels and buttons on the screen + constraints. Sorted all by folders + started to prepare the model with questions & answers

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Денис on 21/05/2023.
+//
+
+
+struct Question {
+    let title: String
+    let responseType: ResponseType
+    let answers: [Answer]
+}
+
+enum ResponseType {
+
+}
+
+struct Answer {
+    
+}
